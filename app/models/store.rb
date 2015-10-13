13b3c2fd772114512e880ac7640c97ef188a9e1f
selfup <boudinot.regis@yahoo.com>
2015-10-13 10:34:07 -0600

    A lot of sweet changes to stoe items views

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,3 +1,14 @@
 class Store < ActiveRecord::Base
   has_many :items
+
+  validates :slug, uniqueness: true, presence: true
+  validates :name, presence: true
+
+  before_validation :generate_slug
+
+  # This will generate the slug and parameterize it for you
+  def generate_slug
+    self.slug = name.parameterize
+  end
+
 end

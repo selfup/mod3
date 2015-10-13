b4eaa3e498892e623c1a7baad40fc09d83764065
selfup <boudinot.regis@yahoo.com>
2015-10-13 10:59:49 -0600

    clarification

--- app/controllers/stores/items_controller.rb
+++ app/controllers/stores/items_controller.rb
@@ -2,4 +2,6 @@ class Stores::ItemsController < Stores::StoresController
   def index
     @items = current_store.items
   end
+
+  #private current_store method is in the Stores::StoreController
 end

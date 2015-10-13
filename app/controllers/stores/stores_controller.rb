48c2ef85aeb2337bddcf54185827a7c8bcd30117
selfup <boudinot.regis@yahoo.com>
2015-10-13 11:05:18 -0600

    added store_not_found method in Stores Stores Controller

--- app/controllers/stores/stores_controller.rb
+++ app/controllers/stores/stores_controller.rb
@@ -1,7 +1,12 @@
 class Stores::StoresController < ApplicationController
+  before_action :store_not_found
   helper_method :current_store
 
   def current_store
     @current_store ||= Store.find_by(slug: params[:store])
+  end
+
+  def store_not_found
+    reirect_to root_path unless current_store
   end
 end

f5a702a5d5daf3344205e97a0af476ad42d2f5f0
selfup <boudinot.regis@yahoo.com>
2015-10-13 10:45:35 -0600

    refactored StoresItemsController

--- app/controllers/stores/items_controller.rb
+++ app/controllers/stores/items_controller.rb
@@ -1,6 +1,11 @@
 class Stores::ItemsController < ApplicationController
   def index
-    @store = Store.find_by(slug: params[:store])
-    @items = @store.items
+    @items = current_store.items
+  end
+
+  private
+
+  def current_store
+    @current_store ||= Store.find_by(slug: params[:store])
   end
 end

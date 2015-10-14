518274d9e0c455a7bfa51ab5e14b68c10672c0ad
selfup <boudinot.regis@yahoo.com>
2015-10-14 10:12:37 -0600

    added more logic, re-seeded

--- mod3-c2/app/services/permissions_service.rb
+++ mod3-c2/app/services/permissions_service.rb
@@ -8,5 +8,19 @@ class PermissionsService
   def allow?(controller, action)
     return true if controller == "stores"
     return true if controller == "sessions"
+
+    if user && user.platform_admin?
+      platform_admin_permissions(controller, action)
+    else
+      guest_user_permissions(controller, action)
+    end
+  end
+
+  private
+
+  def platform_admin_permissions(controller, action)
+    return true if controller == "items"
+    return true if controller == "orders"
+    return true if controller == "users"
   end
 end
